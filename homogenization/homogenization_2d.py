8c2ad56c261cc333fb54224a6a8c102ec4188b90
LeiAn2002 <leian2@illinois.edu>
2025-04-07 00:31:52 +0000

    daily_update 2025-04-07

--- homogenization/homogenization_2d.py
+++ homogenization/homogenization_2d.py
@@ -106,6 +106,7 @@ def homogenized_elasticity_matrix_2d(nodes, tri_elems, quad_elems, mat_table):
     eps = sp.sparse.eye(B_0.shape[1]) * 1e-8  # Eliminate the singularity
     A = B_0.T@K_uc@B_0 + eps
     F = B_0.T@K_uc@B_a
+    A = A.tocsc()
     # A_csc = A.tocsc() if not sps.isspmatrix_csc(A) else A
     luA = spla.splu(A)
     D_0 = np.zeros_like(F.toarray() if sps.issparse(F) else F)
