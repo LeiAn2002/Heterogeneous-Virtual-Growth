33adfb20729d175f1fe83523e35ad679e80e5c43
LeiAn2002 <leian2@illinois.edu>
2025-04-08 01:08:49 +0000

    daily_update 2025-04-08

--- scripts/virtual_growth_script.py
+++ scripts/virtual_growth_script.py
@@ -56,12 +56,12 @@ match dim:
         # r_array = np.tile(first_row_r, (mesh_number, 1))
         # r_array = r_array.flatten()
 
-        lower_boundary_v = 0.2
-        upper_boundary_v = 0.2
+        lower_boundary_v = 0.3
+        upper_boundary_v = 0.7
         v_array = np.random.uniform(low=lower_boundary_v, high=upper_boundary_v, size=(mesh_number * mesh_number, 2))
 
         lower_boundary_r = 0
-        upper_boundary_r = 0
+        upper_boundary_r = 0.7
         r_array = np.random.uniform(low=lower_boundary_r, high=upper_boundary_r, size=(mesh_number * mesh_number, ))
 
         # d, m, n = 0.5, 0.75, 0.25
